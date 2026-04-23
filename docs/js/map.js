b21b151d37a3ffcd4933ef335c2533b4a23d7d09
n129bz <n129bz@outlook.com>
2026-04-23 16:08:04 -0500

    Fixed popup positioning

--- docs/js/map.js
+++ docs/js/map.js
@@ -23,24 +23,37 @@ const pinStyle = new ol.style.Style({
     })
 });
 
-// Initialize the Map
-const vectorSource = new ol.source.Vector();
+const viewextent = [-180, -85, 180, 85];
+const viewposition = ol.proj.fromLonLat([-10943627.55904307, 3595051.022827225]);
+const extent = ol.proj.transformExtent(viewextent, 'EPSG:4326', 'EPSG:3857')
 
+const vectorSource = new ol.source.Vector();
+const vectorLayer = new ol.layer.Vector({
+    title: "Affected Addresses",
+    source: vectorSource,
+    visible: true,
+    extent: extent, 
+    zIndex: 14
+});
+
+// Initialize the Map
 const map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({ source: new ol.source.OSM()}),
-        new ol.layer.Vector({ source: vectorSource })
+        vectorLayer
     ],
-    view: new ol.View({ center: [0, 0], zoom: 2 })
+    view: new ol.View({ center: [-10943627.55904307, 3595051.022827225], zoom: 9 })
 });
+
+map.addOverlay(overlay);
 
 /**
  * Add a click handler to the map to render the popup.
  */
 map.on('singleclick', function (evt) {
     const feature = map.forEachFeatureAtPixel(evt.pixel, (feat) => feat);
-
+    
     if (feature) {
         const name = feature.get('name'); 
         const coordinates = feature.getGeometry().getCoordinates();
